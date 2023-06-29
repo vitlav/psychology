9820b38d5011c85e38015495cced5407e5b32adf
dawlisht <dawlisht@student.21-school.ru>
2023-06-30 00:04:45 +0300

    Working...

--- extentions.js
+++ extentions.js
@@ -53,3 +53,34 @@ function SmoothScroll(id, mode = "none", offset = 0) {
         behavior: 'smooth'
     });
 }
+
+var lastScreenPosition = 0;
+
+function OpenBurger() {
+    lastScreenPosition = document.body.getBoundingClientRect().top * -1;
+
+    document.getElementsByClassName("burger")[0].style.width = "100vw";
+    document.getElementsByClassName("burger")[0].style.opacity = "1";
+    document.getElementsByClassName("burger__links")[0].style.transition = "opacity ease-out 0.1s 0.4s";
+    document.getElementsByClassName("burger__links")[0].style.opacity = "1";
+    
+
+    setTimeout(() => {
+        document.body.style.height = "100vh";
+        document.body.style.overflowY = "hidden";
+    }, 400);
+}
+
+function CloseBurger() {
+    document.getElementsByClassName("burger")[0].style.width = "0";
+    document.getElementsByClassName("burger")[0].style.opacity = "0";
+    document.getElementsByClassName("burger__links")[0].style.transition = "opacity ease-out 0.1s";
+    document.getElementsByClassName("burger__links")[0].style.opacity = "0";
+
+    document.body.style.height = "";
+    document.body.style.overflowY = "";
+
+    window.scrollBy({
+        top: lastScreenPosition
+    });
+}
